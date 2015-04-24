102615a8ec0eb3d41d3042c9a081e53227ab5089
mikel10ortiz <mikel10ortiz@gmail.com>
2015-04-24 12:31:41 +0200

    funciona!!!

--- NatueAdventureFinal/src/es/uji/natureadventure/dao/db/ActivityDao.java
+++ NatueAdventureFinal/src/es/uji/natureadventure/dao/db/ActivityDao.java
@@ -4,12 +4,13 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import es.uji.natureadventure.dao.interfaces.IActivityDao;
@@ -21,22 +22,15 @@ public class ActivityDao implements IActivityDao{
 
 	private Connection con;
 	
-	DataSource dataSource;
+	JdbcTemplate jdbcTemplate;
 	
 	@Autowired
 	public void setDataSource(DataSource dataSource){
-		this.dataSource = dataSource;
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 
 	public ActivityDao(){
 		super();
-		try{
-			con = dataSource.getConnection();
-			System.out.println("connection");
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-		
 		
 	}
 	
@@ -136,7 +130,32 @@ public class ActivityDao implements IActivityDao{
 		return a;
 	}
 
+	private static final class ActivityMapper implements RowMapper<Activity> {
+		
+		public  Activity mapRow(ResultSet rs, int rowNum) throws SQLException {
+			Activity a = new Activity();
+			a.setName(rs.getString("name"));
+			a.setDescription(rs.getString("description"));
+			a.setDuration(rs.getDouble("duration"));
+			a.setKind(rs.getString("kind"));
+			a.setDifficulty(rs.getString("difficulty"));
+			a.setMinPersons(rs.getInt("minPersons"));
+			a.setEnabled(rs.getBoolean("enabled"));
+			a.setPrice(rs.getDouble("price"));
+			a.setMaxPersons(rs.getInt("maxPersons"));
+			
+			return a;
+			
+		}
+	}
+	
 	@Override
+	public List<Activity> getActivities(){
+		return this.jdbcTemplate.query("select * from Activity"
+				, new ActivityMapper());
+	}
+	
+	/*@Override
 	public List<Activity> getActivities() {
 		Activity a = null;
 		List<Activity> l = new ArrayList<Activity>();
@@ -168,6 +187,8 @@ public class ActivityDao implements IActivityDao{
 		}
 
 		return l;
-	}
+	}*/
+	
+	
 
 }
